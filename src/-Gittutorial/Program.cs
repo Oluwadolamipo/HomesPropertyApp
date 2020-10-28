003d99ceeaf946749bae5eb4caeec6595e8543d4
Mosuro Dolapo <Oluwadolamipo@macs-MacBook-Pro.local>
2020-10-28 23:02:44 +0100

    added console project files

---
+++ src/-Gittutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Gittutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
